881ac7dba0e3dd909176266e60f61036312f4876
Apex <robsonlawson3@gmail.com>
2025-08-25 16:50:58 +0100

    Initial dice entity implementation

--- src/main/java/dev/apexstudios/fantasydice/client/FantasyDiceClientEntryPoint.java
+++ src/main/java/dev/apexstudios/fantasydice/client/FantasyDiceClientEntryPoint.java
@@ -1,9 +1,11 @@
 package dev.apexstudios.fantasydice.client;
 
 import dev.apexstudios.fantasydice.FantasyDice;
+import dev.apexstudios.fantasydice.client.renderer.DiceEntityRenderer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterSelectItemModelPropertyEvent;
 
 @Mod(value = FantasyDice.ID, dist = Dist.CLIENT)
@@ -13,5 +15,7 @@ public final class FantasyDiceClientEntryPoint {
             event.register(FantasyDice.identifier("sides"), DiceSidesSelectModelProperty.TYPE);
             event.register(FantasyDice.identifier("material"), DiceMaterialSelectModelProperty.TYPE);
         });
+
+        modBus.addListener(EntityRenderersEvent.RegisterRenderers.class, event -> event.registerEntityRenderer(FantasyDice.DICE_ENTITY.value(), DiceEntityRenderer::new));
     }
 }
