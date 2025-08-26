7a09bf214730142e18bd67a3d9f189a5507316b2
Apex <robsonlawson3@gmail.com>
2025-08-26 22:36:38 +0100

    Remove shake anim from dice item

--- src/main/java/dev/apexstudios/fantasydice/DiceItem.java
+++ src/main/java/dev/apexstudios/fantasydice/DiceItem.java
@@ -12,7 +12,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.level.Level;
@@ -25,28 +24,14 @@ public final class DiceItem extends Item implements CustomCooldownGroup {
 
     @Override
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
-        player.startUsingItem(hand);
-        return InteractionResult.CONSUME;
-    }
+        var stack = player.getItemInHand(hand);
 
-    @Override
-    public ItemUseAnimation getUseAnimation(ItemStack stack) {
-        return ItemUseAnimation.BRUSH;
-    }
+        if(throwDice(level, stack, player)) {
+            player.getCooldowns().addCooldown(stack, SharedConstants.TICKS_PER_SECOND/2);
+            return InteractionResult.SUCCESS;
+        }
 
-    @Override
-    public int getUseDuration(ItemStack stack, LivingEntity entity) {
-        return SharedConstants.TICKS_PER_SECOND * 10;
-    }
-
-    @Override
-    public boolean releaseUsing(ItemStack stack, Level level, LivingEntity entity, int timeLeft) {
-        var heldTime = stack.getUseDuration(entity) - timeLeft;
-
-        if(heldTime < SharedConstants.TICKS_PER_SECOND / 2)
-            return false;
-
-        return throwDice(level, stack, entity);
+        return super.use(level, player, hand);
     }
 
     @Override
