24ef00f1ca4f221c45b676c5fc265bd1907602a5
Apex <robsonlawson3@gmail.com>
2026-02-12 18:25:45 +0000

    Initial update to support Registree v2

--- src/main/java/dev/apexstudios/fantasydice/client/FantasyDiceClientEntryPoint.java
+++ src/main/java/dev/apexstudios/fantasydice/client/FantasyDiceClientEntryPoint.java
@@ -1,12 +1,9 @@
 package dev.apexstudios.fantasydice.client;
 
-import dev.apexstudios.fantasydice.client.renderer.DiceEntityRenderer;
 import dev.apexstudios.fantasydice.common.FantasyDice;
-import dev.apexstudios.fantasydice.common.util.DiceRegistries;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterSelectItemModelPropertyEvent;
 
 @Mod(value = FantasyDice.ID, dist = Dist.CLIENT)
@@ -16,7 +13,5 @@ public final class FantasyDiceClientEntryPoint {
             event.register(FantasyDice.identifier("sides"), DiceSidesSelectModelProperty.TYPE);
             event.register(FantasyDice.identifier("material"), DiceMaterialSelectModelProperty.TYPE);
         });
-
-        modBus.addListener(EntityRenderersEvent.RegisterRenderers.class, event -> event.registerEntityRenderer(DiceRegistries.DICE_ENTITY.value(), DiceEntityRenderer::new));
     }
 }
--- src/main/java/dev/apexstudios/fantasydice/common/util/DiceRegistries.java
+++ src/main/java/dev/apexstudios/fantasydice/common/util/DiceRegistries.java
@@ -1,12 +1,17 @@
 package dev.apexstudios.fantasydice.common.util;
 
+import dev.apexstudios.fantasydice.client.renderer.DiceEntityRenderer;
 import dev.apexstudios.fantasydice.common.DiceEntity;
 import dev.apexstudios.fantasydice.common.FantasyDice;
-import dev.apexstudios.registree.api.Registree;
-import dev.apexstudios.registree.api.holder.DeferredDataComponent;
-import dev.apexstudios.registree.api.holder.DeferredEntity;
-import dev.apexstudios.registree.api.holder.DeferredGameRule;
-import dev.apexstudios.registree.api.holder.DeferredItem;
+import dev.apexstudios.registree.Registree;
+import dev.apexstudios.registree.holder.DeferredDataComponentType;
+import dev.apexstudios.registree.holder.DeferredEntityType;
+import dev.apexstudios.registree.holder.DeferredGameRule;
+import dev.apexstudios.registree.registrar.CreativeModeTabRegistrar;
+import dev.apexstudios.registree.registrar.DataComponentTypeRegistrar;
+import dev.apexstudios.registree.registrar.EntityTypeRegistrar;
+import dev.apexstudios.registree.registrar.GameRuleRegistrar;
+import dev.apexstudios.registree.registrar.ItemRegistrar;
 import net.minecraft.SharedConstants;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.MobCategory;
@@ -14,23 +19,35 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.gamerules.GameRuleCategory;
+import net.neoforged.neoforge.registries.DeferredItem;
 
 public interface DiceRegistries {
     Registree REGISTREE = Registree.create(FantasyDice.ID);
+    ItemRegistrar ITEMS = REGISTREE.items();
+    EntityTypeRegistrar ENTITY_TYPES = REGISTREE.entityTypes();
+    GameRuleRegistrar GAME_RULES = REGISTREE.gameRules();
+    DataComponentTypeRegistrar DATA_COMPONENT_TYPES = REGISTREE.dataComponentTypes();
+    CreativeModeTabRegistrar CREATIVE_MODE_TABS = REGISTREE.creativeModeTabs();
 
-    DeferredDataComponent<String> MATERIAL_COMPONENT = REGISTREE.registerDataComponent("material", DiceCodecs.MATERIAL_CODEC);
-    DeferredDataComponent<Integer> SIDES_COMPONENT = REGISTREE.registerDataComponent("sides", DiceCodecs.SIDES_CODEC);
-    DeferredItem<Item> DICE_ITEM = REGISTREE.registerItem("dice", Item::new, properties -> properties.stacksTo(8));
+    DeferredDataComponentType<String> MATERIAL_COMPONENT = DATA_COMPONENT_TYPES.register("material", DiceCodecs.MATERIAL_CODEC);
+    DeferredDataComponentType<Integer> SIDES_COMPONENT = DATA_COMPONENT_TYPES.register("sides", DiceCodecs.SIDES_CODEC);
 
-    DeferredEntity<DiceEntity> DICE_ENTITY = REGISTREE.registerEntity("dice", DiceEntity::new, MobCategory.MISC, properties -> properties
-            .noLootTable()
-            .sized(.25F, .25F)
-            .eyeHeight(ItemEntity.EYE_HEIGHT)
-            .clientTrackingRange(6)
-            .updateInterval(SharedConstants.TICKS_PER_SECOND)
-    );
+    DeferredItem<Item> DICE_ITEM = ITEMS.builder("dice", Item::new)
+            .properties(properties -> properties.stacksTo(8))
+            .register();
 
-    ResourceKey<CreativeModeTab> CREATIVE_MODE_TAB = REGISTREE.registerCreativeModeTab("dice", () -> Dice.create(Dice.DEFAULT_SIDES[0], Dice.DEFAULT_MATERIALS[0]).create(), (parameters, output) -> {
+    DeferredEntityType<DiceEntity> DICE_ENTITY = ENTITY_TYPES.<DiceEntity>builder("dice", DiceEntity::new, MobCategory.MISC)
+            .properties(properties -> properties
+                    .noLootTable()
+                    .sized(.25F, .25F)
+                    .eyeHeight(ItemEntity.EYE_HEIGHT)
+                    .clientTrackingRange(6)
+                    .updateInterval(SharedConstants.TICKS_PER_SECOND)
+            )
+            .renderer(() -> () -> DiceEntityRenderer::new)
+            .register();
+
+    ResourceKey<CreativeModeTab> CREATIVE_MODE_TAB = CREATIVE_MODE_TABS.register("dice", () -> Dice.create(Dice.DEFAULT_SIDES[0], Dice.DEFAULT_MATERIALS[0]), (parameters, output) -> {
         for(var material : Dice.DEFAULT_MATERIALS) {
             for(var side : Dice.DEFAULT_SIDES) {
                 output.accept(Dice.create(side, material).create());
@@ -38,5 +55,5 @@ public interface DiceRegistries {
         }
     });
 
-    DeferredGameRule<Integer> RULE_DICE_LIFETIME = REGISTREE.registerIntegerGameRule("dice_lifetime", GameRuleCategory.MISC, DiceEntity.DEFAULT_LIFETIME, 0);
+    DeferredGameRule<Integer> RULE_DICE_LIFETIME = GAME_RULES.registerInteger("dice_lifetime", GameRuleCategory.MISC, DiceEntity.DEFAULT_LIFETIME);
 }
